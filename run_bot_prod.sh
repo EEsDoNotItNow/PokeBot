ea75cefc626c8fcb522987065a055064d669027a
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-09 15:47:37 -0600

    Update working credentials

--- run_bot_prod.sh
+++ run_bot_prod.sh
@@ -4,5 +4,5 @@
 # export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
 
 git pull
-source ~/.ssh/discord_bot.prod.creds
+source ~/.ssh/pokebot.discord.creds
 pipenv run python ./Bot.py --env prod "$@"
