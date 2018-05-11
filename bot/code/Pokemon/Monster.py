a34b05b803931737ce633fddcf6b27e6dae2d303
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-10 23:39:09 -0600

    Initial work on Monster class, much to be done

--- bot/code/Pokemon/Monster.py
+++ bot/code/Pokemon/Monster.py
@@ -1,5 +1,27 @@
 
+from ..Log import Log
+from ..SQL import SQL
+
 from .Pokemon import Pokemon
 
 class Monster(Pokemon):
-    pass
+
+    log = Log()
+    sql = SQL()
+
+    def __init__(self, pokemon_id, monster_id=None):
+        """Create a new Monster (an acutal pokemon in the sim)
+
+        @param pokemon_id Id from the pokedex table, stats are based off this value
+        @param monster_id if given, load all other values from the DB
+        """
+
+        # Init our superclass
+        super().__init__(pokemon_id)
+
+        self.hp = self.base_hp
+        self.xp = 0
+
+        if monster_id:
+            # TODO: Load from SQL
+            raise NotImplementedError()
