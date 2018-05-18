c352b77b1afc58ad16f14d2e3155b6c3f3340a79
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-18 09:19:20 -0600

    :wrench: Don't generate binaries in dev

--- run_bot_dev.sh
+++ run_bot_dev.sh
@@ -3,4 +3,4 @@
 # export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
 
 source ~/.ssh/pokebot.discord.creds
-pipenv run python ./Bot.py --env dev "$@"
+pipenv run python -B ./Bot.py --env dev "$@"
