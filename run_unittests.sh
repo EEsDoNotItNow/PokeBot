73d80b8bd9a0379a1e19d49055be6513d4732658
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-18 11:45:32 -0600

    Try to get pipenv working again

--- run_unittests.sh
+++ run_unittests.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# .creds file should look like this
-# export CLIENT_TOKEN="LOTS OF NUMBERS THAT MAKE A TOKEN GO HERE"
-
-source ~/.ssh/pokebot.discord.creds
+pipenv install --dev
 pipenv run nosetests --timer-ok 1s --timer-warning 5s --with-timer
