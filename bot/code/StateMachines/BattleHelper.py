26bbec726ca14d005e8e94831bde3ddab42f815b
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-21 23:39:23 -0600

    Add EventBase to begin working on an eventing system

--- bot/code/StateMachines/BattleHelper.py
+++ bot/code/StateMachines/BattleHelper.py
@@ -5,42 +5,43 @@ from ..Singleton import SingletonArgs
 
 
 
-class BattleHelper:
+class BattleHelper(metaclass=SingletonArgs):
 
 
-    def __init__(self, battle_id, turn):
+    def __init__(self, battle_id, encounter=False, trainer=False, players=False):
         self.battle_id = battle_id
-        self.turn = turn
+        self.turn = 0
 
         self.participants = []
 
         self.effects = []
 
-        self.has_run = False
-
-        self._log = []
+        self._log = {}
 
 
-    ## Add a monster to the planned execution
-    # @param monster Monster to be added (order doesn't matter)
-    # @param move Move number to execute
-    async def register(self, element, mod=None):
-        if type(element) == Monster:
-            if monster not in [x[0] for x in self.participants]:
-                self.participants.append((monster,move))
-        else:
-            pass
+    async def register(self, trainer):
+        """Register a trainer with the battle
+        """
+        pass
 
 
     async def deregister(self, monster):
         self.participants = [x for x in self.participants if x[0] is not mosnter]
 
 
-    async def get_log(self):
+    async def get_log(self, full=False):
         return self._log
 
 
+    async def mark_ready(self, trainer):
+        """
+        Mark yourself as read for the turn to begin. 
+        """
+
     async def execute(self):
+        """
+        Simulate the turn
+        """
         if self.has_run:
             return
         self.has_run = True
---
+++ bot/code/StateMachines/EventBase.py
@@ -0,0 +1,16 @@
+
+
+
+class EventBase:
+    """Define the base event for battle events. 
+    """
+
+    
+    def __init__(self):
+
+        # Mark when event has been used.
+        self.triggered = False
+
+        # 
+        self.turns = -1
+        pass
