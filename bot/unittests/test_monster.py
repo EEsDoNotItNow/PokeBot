2c11bf8d53e42f3bc0379925c66325a00971bbb7
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-22 12:11:54 -0600

    :new: Add new (very slow) unit test for try different monster levels

--- bot/unittests/test_monster.py
+++ bot/unittests/test_monster.py
@@ -23,7 +23,8 @@ class test_monster(unittest.TestCase):
     def tearDown(self):
         del self.sql
 
-    def test_create_all_monsters(self):
+
+    def test_00_create_all_monsters(self):
         for i in range(1, 807 + 1):
             with self.subTest(pokemon_id=i):
                 poke = Monster(pokemon_id=i)
@@ -45,7 +46,39 @@ class test_monster(unittest.TestCase):
                 # We cannot run async calls from here, use _run
                 self.assertEqual(str(poke.pokemon_id), str(i))
 
-    def test_save_and_load_monsters(self):
+
+    def test_01_create_all_monsters_all_levels(self):
+        for i in range(1, 807 + 1):
+            for level in range(1, 101):
+                with self.subTest(pokemon_id=i, level=level):
+                    poke = Monster(pokemon_id=i)
+                    _run(poke.load())
+
+                    growth_rate_id = poke.growth_rate_id
+
+                    cmd = """
+                        SELECT experience
+                        FROM experience_lookup
+                        WHERE
+                            growth_rate_id=:growth_rate_id
+                            AND level=:level
+                    """
+                    poke.xp = self.sql.cur.execute(cmd, locals()).fetchone()['experience']
+
+                    poke.iv_hp = np.random.randint(0, 31)
+                    poke.iv_attack = np.random.randint(0, 31)
+                    poke.iv_defense = np.random.randint(0, 31)
+                    poke.iv_sp_attack = np.random.randint(0, 31)
+                    poke.iv_sp_defense = np.random.randint(0, 31)
+                    poke.iv_speed = np.random.randint(0, 31)
+
+                    _run(poke.update_state())
+
+
+                    self.assertEqual(level, poke.level)
+
+
+    def test_03_save_and_load_monsters(self):
         for i in range(1, 807 + 1):
             poke = Monster(pokemon_id=i)
 
