6f6775a218afef2d8cabe337991d20d8fbae03f7
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-24 22:08:11 -0600

    Missed move_slots in last push

--- bot/unittests/test_move_slots.py
+++ bot/unittests/test_move_slots.py
@@ -18,7 +18,7 @@ class test_move_slots(unittest.TestCase):
 
 
     def test_import_all_move_slots(self):
-        """Load up every Move in the DB
+        """Load up every MoveSlot in the DB
         """
         for i in range(1, 728 + 1):
             move_slot = MoveSlot(move_id=i)
