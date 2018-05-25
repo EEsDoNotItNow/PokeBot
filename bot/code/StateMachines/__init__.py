a6d59700c8ebbb74fd0a5f8a8b36278bb9ccb97e
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-25 12:16:24 -0600

    Rename several files to UserInterface

--- bot/code/StateMachines/__init__.py
+++
@@ -1,7 +0,0 @@
-
-from .BattleStateMachine import BattleStateMachine
-from .EncounterStateMachine import EncounterStateMachine
-from .NewPlayerStateMachine import NewPlayerStateMachine
-
-
-__all__ = [BattleStateMachine, EncounterStateMachine, NewPlayerStateMachine]
---
+++ bot/code/UserInterfaces/__init__.py
@@ -0,0 +1,7 @@
+
+from .BattleUserInterface import BattleUserInterface
+from .EncounterUserInterface import EncounterUserInterface
+from .NewPlayerUserInterface import NewPlayerUserInterface
+
+
+__all__ = [BattleUserInterface, EncounterUserInterface, NewPlayerUserInterface]
