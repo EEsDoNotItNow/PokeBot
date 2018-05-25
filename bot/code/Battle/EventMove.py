549ba3c69ed63ca3228576bf1c4721fcb1ba886f
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-24 23:52:01 -0600

    Update EventMove to know more about what it needs to do

--- bot/code/Battle/EventMove.py
+++ bot/code/Battle/EventMove.py
@@ -9,10 +9,12 @@ from .EventBase import EventBase
 class EventMove(EventBase):
 
 
-    def __init__(self, move):
+    def __init__(self, move, user, target=None):
         """Given a Move object, create a new even to handle this attack.
         """
         self.move = move
+        self.user = user
+        self.target = target
 
 
     def execute(self, battle, *args, **kwargs):
