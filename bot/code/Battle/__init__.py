41f57ff9d385e06c457148e9b37e8b7ef097562e
EEsDoNotItNow <eesdonotitnow@gmail.com>
2018-05-27 12:09:24 -0600

    Missed a cleanup of imports

--- bot/code/Battle/__init__.py
+++ bot/code/Battle/__init__.py
@@ -1,12 +1,8 @@
 
 from .Battle import Battle
 from .BattleManager import BattleManager
-from .Event.EventBase import EventBase
-from .Event.EventRun import EventRun
 
 __all__ = [
     Battle,
     BattleManager,
-    EventBase,
-    EventRun,
 ]
